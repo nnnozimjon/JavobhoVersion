129301f451d0d33285e50d8132c0dc33452587a1
abdullojr <iamnozimjon@gmail.com>
2023-04-01 02:44:49 +0500

    +

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -4,20 +4,25 @@ import React from 'react'
 import IIcon from './interface'
 import list from './svg-list'
 
-const Icon: React.FC<IIcon.props> = React.memo(
-  ({ size = 24, name, className, onClick }: IIcon.props) => (
-    <svg
-      viewBox="0 0 24 24"
-      style={{ width: size + 'px', height: size + 'px' }}
-      fill="currentColor"
-      className={className}
-      onClick={onClick}
-    >
-      {list[name]}
-    </svg>
-  ),
-  (prevProps, nextProps) =>
-    prevProps.name === nextProps.name && prevProps.size === nextProps.size
-)
+const Icon: React.FC<IIcon.props> = ({
+  size = 24,
+  name,
+  className,
+  onClick,
+}: IIcon.props) => {
+  return (
+    <>
+      <svg
+        viewBox="0 0 24 24"
+        style={{ width: size + 'px', height: size + 'px' }}
+        fill="currentColor"
+        className={className}
+        onClick={onClick}
+      >
+        {list[name]}
+      </svg>
+    </>
+  )
+}
 
 export default Icon
