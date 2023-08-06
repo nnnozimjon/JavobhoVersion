2e31f720912fc8205b7c4b25cf9bff3e119deeee
nnnozimjon <iamnozimjon@gmail.com>
2023-08-06 19:19:33 +0500

    +

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { Suspense } from 'react'
 import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
 import Cookies from 'js-cookie'
@@ -7,22 +7,18 @@ import { getUnixTime } from 'date-fns'
 
 import '@/styles/globals.css'
 import store, { SettingsContextProvider } from '@/store'
-import Login from './login'
 import DesktopLayout from '@/components/layout'
 import { UserContextProvider } from '@/store/contexts/UserContect'
 
-export default function App({ Component, pageProps }: AppProps) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+// Lazy loading the Login component
+const Login = React.lazy(() => import('./login'))
 
-  useEffect(() => {
-    const access_token = Cookies.get('access_token') || ''
-    setIsLoggedIn(checkToken(access_token))
-  }, [])
+const App = ({ Component, pageProps }: AppProps) => {
+  const access_token = Cookies.get('access_token') || ''
 
-  function checkToken(token: string) {
+  const checkToken = (token: string) => {
     try {
       const decode: any = jwtDecode(token || '')
-
       const { exp, username, userId } = decode
       return (
         getUnixTime(new Date()) < exp! &&
@@ -34,27 +30,27 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }
 
+  const isTokenValid = checkToken(access_token)
+
   return (
     <Provider store={store}>
       <UserContextProvider>
         <SettingsContextProvider>
-          {isLoggedIn ? (
-            <DesktopLayout>
-              <InnerApp>
+          <Suspense fallback={<div>Loading...</div>}>
+            {isTokenValid ? (
+              <Login />
+            ) : (
+              <DesktopLayout>
                 <div className="h-full overflow-y-scroll scrollbar-hide ">
                   <Component {...pageProps} />
                 </div>
-              </InnerApp>
-            </DesktopLayout>
-          ) : (
-            <Login />
-          )}
+              </DesktopLayout>
+            )}
+          </Suspense>
         </SettingsContextProvider>
       </UserContextProvider>
     </Provider>
   )
 }
 
-const InnerApp: React.FC<any> = ({ children }) => {
-  return <>{children}</>
-}
+export default App
